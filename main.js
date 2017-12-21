b4f067e5ad28893b06388553313091ba747dcd9b
mk7087 <yamk4054@outlook.com>
2017-12-21 13:17:52 +0900

    Added processing to handle config file

--- main.js
+++ main.js
@@ -1,7 +1,20 @@
 if( [ 'win32', 'linux', 'darwin' ].indexOf( process.platform.toLowerCase() ) == -1 ){ console.log('This platform is not supported'); console.log('If you stil want it please comment out or delete the first line of main.js'); process.exit( 1 ); }
+
 var http          = require( 'http' );
+var fs            = require( 'fs' );
+
 var port     = 80;
 var hostname = 'localhost';
+
+//Config file
+
+var config = JSON.parse(fs.readFileSync( 'config.json', 'utf-8' ));
+if( config.http === undefined ){
+    config.http = {};
+}
+port = config.http.port === undefined ? port : config.http.port;
+hostname = config.http.hostname === undefined ? hostname : config.http.hostname;
+
 //Arguments
 for( var i = 2; i < process.argv.length; i++ ){
     if( process.argv[ i ].match( /^-*.$/ ) ){
@@ -20,6 +33,8 @@ for( var i = 2; i < process.argv.length; i++ ){
 //================================
 
 var server = http.createServer();
+
 server.on( 'request', function( http_request, http_response ){
 })
+
 server.listen( port, hostname );
